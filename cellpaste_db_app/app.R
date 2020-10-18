447cb857b17d480cf8842830b9a04660b7bb6a82
James Welch <jwelch1123@gmail.com>
2020-10-18 12:57:52 -0400

    removed the 'on session end, stop app' so that I could reload once a submission is made. broke the database into two sheets for a current inventory and a historic ledger, changed several of the inputs to make it more clear

--- cellpaste_db_app/app.R
+++ cellpaste_db_app/app.R
@@ -11,18 +11,21 @@ ui <- fluidPage(
     titlePanel("Cell Paste Database"),
     
     
-    tabsetPanel( tabPanel("Database", 
+    tabsetPanel( tabPanel("Current Inventory", 
                           mainPanel(
-                              dataTableOutput('cell_db_table'))
+                              dataTableOutput("inventory_db_table"))
                           ), 
-                 tabPanel("New Batch Entry", 
+                 
+                tabPanel("Overall Ledger", 
+                          mainPanel(
+                              dataTableOutput("ledger_db_table"))
+                          ), 
+                 
+                 tabPanel("New Batch Input", 
+                          br(),
                           dateInput(inputId = "date_action",
                                     label = "Date of Action",
                                     format = "yymmdd"),
-                          
-                          selectInput(inputId = "mod_type",
-                                      label = "Type of Change",
-                                      c("Entry","Removal","Balance")),
 
                           textInput(inputId = "batch_id",
                                     label = "Batch ID (PB######-SU1,2,3)"),
@@ -56,14 +59,34 @@ ui <- fluidPage(
 
                           textInput(inputId = "reason",
                                      label = "Reason"),
-                          
+                          br(),
+                          br(),
                           actionButton(inputId = "upload_entry",
                                        label = "Submit to Database"),
                           br(),
                           br()
                           ), 
                  
-                 tabPanel("Graphics?", "contents"))
+                 tabPanel("Batch Modification", 
+                          dateInput(inputId = "date_action",
+                                    label = "Date of Action",
+                                    format = "yymmdd"),
+                          
+                          selectInput(inputId = "mod_type",
+                                      label = "Type of Change",
+                                      c("Removal","Set Balance"),
+                                      selected = "Removal"),
+                          
+                          selectizeInput(inputId = "unique_batch_id",
+                                         label = "Unique Batch ID (PB + bag #)",
+                                         choices = NULL,
+                                         selected = NULL)
+                                        
+                          ),
+                
+                 tabPanel("graphics","contents")
+                 
+    )
 )
     
     
@@ -72,26 +95,33 @@ ui <- fluidPage(
     
 
 # Shiny App Server ####
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
     #ends app run when page is closed in browser
     #Thanks to the following for the tip:
     #https://stackoverflow.com/questions/35306295/how-to-stop-running-shiny-app-by-closing-the-browser-window
-    onSessionEnded(function() {stopApp()})
+
+    # Ending the session cleanly like this doesn't allow
+    # the page to be reloaded when bag is submitted.
+    #onSessionEnded(function() {stopApp()})
+
+    #Inport statements ####
+    inventory_db <- read_csv("./app_inventory.csv") %>% 
+        transform(Date_Modified = as.Date(Date_Modified,"%m/%d/%y"))
     
-    #Import statement ####
-    cell_db <- read_csv("./app_demo_data.csv") %>% 
-        transform(Date_Added = as.Date(Date_Added,"%m/%d/%y"))
+    ledger_db <- read_csv("./app_ledger.csv") %>% 
+        transform(Date_Added = as.Date(Date_Added, "%m/%d/%y"))
     
-    output$cell_db_table <- DT::renderDataTable(cell_db)
+    output$inventory_db_table <- DT::renderDataTable(inventory_db)
+    output$ledger_db_table <- DT::renderDataTable(ledger_db)
     
-    #Save the database entry ####
+    
+    #Material Entry update ####
     #observe event and save data to new row and re-save dataframe
     observeEvent(input$upload_entry,{
         
         #c() is wrong, it coerses the same class for all values
-        submission_list <- list(input$date_action,
-                             input$mod_type,
+        ledger_update_list <- list(input$date_action,
+                             "Entry",
                              input$batch_id,
                              input$bag_number,
                              paste0(input$batch_id,"_",input$bag_number),
@@ -101,13 +131,55 @@ server <- function(input, output, session) {
                              input$operator_name,
                              input$freezer_name,
                              input$shelf_number,
-                             input$reason)
-
-        transistion_db <- rbind(cell_db, submission_list) %>% 
+                             "Batch Harvest")
+        
+        ledger_update_db <- rbind(ledger_db, ledger_update_list) %>% 
             transform(Date_Added = as.character(Date_Added,"%m/%d/%y"))
-        write.csv(transistion_db, file = "./app_demo_data.csv", row.names = F)
-    })
+        
+        write.csv(ledger_update_db, file = "./app_ledger.csv", row.names = F)
+    
+        inventory_update_list <- list(input$date_action,
+                                      paste0(input$batch_id,"_",input$bag_number),
+                                      input$batch_id,
+                                      input$bag_number,
+                                      input$strain_id,
+                                      input$material_type,
+                                      input$sample_weight, #needs update
+                                      input$freezer_name,
+                                      input$shelf_number)        
 
+        inventory_update_db <- rbind(inventory_db, inventory_update_list) %>% 
+            transform(Date_Added = as.character(Date_Added,"%m/%d/%y"))
+        
+        write.csv(inventory_update_db, file = "./app_inventory.csv", row.names = F)        
+        
+        #reload the page to show people things happened
+        #not elegant but functional.
+        session$reload()
+    
+    })
+    
+    # Mod Update ####
+    # observeEvent(input$upload_modification,{
+    #     inventory_update_list <- list(input$date_action,
+    #                                   paste0(input$batch_id,"_",input$bag_number),
+    #                                   input$batch_id,
+    #                                   input$bag_number,
+    #                                   input$strain_id,
+    #                                   input$material_type,
+    #                                   input$sample_weight, #needs update
+    #                                   input$freezer_name,
+    #                                   input$shelf_number)
+    # 
+    # 
+    # })
+    
+    # Selectize Updates
+    
+    #unique_batches <- updateSelectizeInput(session, "unique_batch_id", choices = inventory_db$Unique_Bag_ID)
+    updateSelectizeInput(session, "unique_batch_id", 
+                         choices = inventory_db$Unique_Bag_ID, 
+                         selected = "", server = TRUE)
 }
 
 # Run the application 
