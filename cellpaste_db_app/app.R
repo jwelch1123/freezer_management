ac3b31c0427460dc19cc1fb309ba0e89fa0d0d06
James Welch <jwelch1123@gmail.com>
2020-11-07 17:13:17 -0500

    Updated several inputs to be more user friendly, started to add graph, current issue is date selection and strain selection

--- cellpaste_db_app/app.R
+++ cellpaste_db_app/app.R
@@ -2,25 +2,26 @@
 library(shiny)
 library(tidyverse)
 library(DT)
-
+library(ggplot2)
 
 # Shiny App UI ####
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
+    # Panels ####
     titlePanel("Cell Paste Database"),
     
-    
+                # Inventory ####
     tabsetPanel( tabPanel("Current Inventory", 
                           mainPanel(
                               dataTableOutput("inventory_db_table"))
                           ), 
-                 
+                # Ledger #### 
                 tabPanel("Overall Ledger", 
                           mainPanel(
                               dataTableOutput("ledger_db_table"))
                           ), 
-                 
+                 # Batch Input ####
                  tabPanel("New Batch Input", 
                           br(),
                           dateInput(inputId = "date_action",
@@ -28,10 +29,10 @@ ui <- fluidPage(
                                     format = "yymmdd"),
 
                           textInput(inputId = "batch_id",
-                                    label = "Batch ID (PB######)"), #FLAG
+                                    label = "Batch ID (PB######)"),
                           
                           checkboxGroupInput(inputId = "input_su_checkboxes",
-                                             label = "Select Relavent SU units",
+                                             label = "Select Source SU units",
                                              choiceNames = paste0("SU",1:6),
                                              choiceValues = 1:6,
                                              inline = T),
@@ -50,18 +51,27 @@ ui <- fluidPage(
                                        label = "Type of Material",
                                        choices = c("Pellet","Ferm-Sup")),
                           
-                          textInput(inputId = "strain_id",
-                                    label = "Strain ID (PP##)"),
-
+                          selectizeInput(inputId = "strain_id",
+                                         label = "Strain ID (PP##)",
+                                         choices = NULL,
+                                         selected = NULL,
+                                         options = list( create = TRUE,
+                                                         createOnBlur = TRUE)),
+                          
                           selectizeInput(inputId = "operator_name",
                                     label = "Operator Name",
                                     choices = NULL,
                                     selected = NULL,
                                     options = list( create = TRUE,
                                                     createOnBlur = TRUE)),
-
-                          textInput(inputId = "freezer_name",
-                                    label = "Freezer Name"),
+                          
+                          selectizeInput(inputId = "freezer_name",
+                                         label = "Freezer Name",
+                                         choices = NULL,
+                                         selected = NULL,
+                                         options = list( create = TRUE,
+                                                         createOnBlur = TRUE)),
+                          
                           numericInput(inputId = "shelf_number",
                                         label = "Shelf from Top (top = 1)",
                                         value = 1,
@@ -73,7 +83,7 @@ ui <- fluidPage(
                           br(),
                           br()
                           ), 
-                 
+                 # Batch Modification ####
                  tabPanel("Batch Modification", 
                           dateInput(inputId = "mod_date_action",
                                     label = "Date of Action",
@@ -81,11 +91,11 @@ ui <- fluidPage(
                           
                           selectInput(inputId = "mod_type",
                                       label = "Type of Change",
-                                      c("Removal","Set Balance"), #change?
+                                      c("Removal","Set Balance"),
                                       selected = "Removal"),
                           
                           selectizeInput(inputId = "mod_unique_batch_id",
-                                         label = "Unique Batch ID (PB + bag #)",
+                                         label = "Unique Batch ID (PB _ bag #)",
                                          choices = NULL,
                                          selected = NULL),
                           
@@ -100,6 +110,13 @@ ui <- fluidPage(
                                       options = list( create = TRUE,
                                                       createOnBlur = TRUE) ),
                           
+                          selectizeInput(inputId = "mod_op_group",
+                                         label = "Group of Operator (DSP, Experimental, etc)",
+                                         choices = NULL,
+                                         selected = NULL,
+                                         options = list( create = TRUE,
+                                                         createOnBlur = TRUE) ),
+                          
                           textInput(inputId = "mod_reason",
                                       label = "Reason for Change",
                                       value = ""),
@@ -110,29 +127,37 @@ ui <- fluidPage(
                           br(),
                           br(),
                           actionButton(inputId = "mod_update_entry",
-                                       label = "Submit Change to Database"),                          
+                                       label = "Submit Change to Database")                          
                           ),
-                
-                 tabPanel("graphics","contents")
+                 # Graphics ####
+                 tabPanel("Strain Overview","By Strain, look at current stored batchs. Looks at overall production, 
+                          pie chart of usage by group. ",
+
+                          selectizeInput(inputId = "s_overview_strain_id",
+                                         label = "Select Strain to View",
+                                         choices = NULL#,
+                                         #selected = 
+                                             ),
+                          dateRangeInput(inputId = "s_overview_daterange",
+                                         label = "Select a Date Range to View"),
+                          plotOutput(outputId = "strain_overview_plot"),
+                          
+                          br(),
+                          "ending Text"
+                          ),
+                 tabPanel("Batch Overview","Explore by Batch, see all modifications, current inventory and ledger"),
+                 tabPanel("Search","Bring up all information on selected batches")
                  
     )
 )
     
     
-    #Thanks to ____ for this introduction which I adapted.
+    #Thanks to this source for this which I adapted.
     #https://deanattali.com/2015/06/14/mimicking-google-form-shiny/
     
 
 # Shiny App Server ####
 server <- function(input, output, session) {
-    #ends app run when page is closed in browser
-    #Thanks to the following for the tip:
-    #https://stackoverflow.com/questions/35306295/how-to-stop-running-shiny-app-by-closing-the-browser-window
-
-    # Ending the session cleanly like this doesn't allow
-    # the page to be reloaded when bag is submitted.
-    #onSessionEnded(function() {stopApp()})
-
     #Inport statements ####
     inventory_db <- read_csv("./app_inventory.csv") %>% 
         transform(Date_Modified = as.Date(Date_Modified,"%m/%d/%y"))
@@ -140,23 +165,23 @@ server <- function(input, output, session) {
     ledger_db <- read_csv("./app_ledger.csv") %>% 
         transform(Date_Added = as.Date(Date_Added, "%m/%d/%y"))
     
+    # Render Tables ####
     output$inventory_db_table <- DT::renderDataTable(inventory_db)
     output$ledger_db_table <- DT::renderDataTable(ledger_db)
     
     
     # Entry update ####
-    #observe event and save data to new row and re-save dataframe
+    #observe Batch Addition and save data to new row and re-save dataframe
     observeEvent(input$upload_entry,{
         
-        #c() is wrong, it coerses the same class for all values
         ledger_update_list <- list(input$date_action,
                              "Entry",
-                             paste0(input$batch_id,"_SU",input$input_su_checkboxes),
+                             paste0(toupper(input$batch_id),"_SU",input$input_su_checkboxes),
                              input$bag_number,
-                             paste0(input$batch_id,"_",input$bag_number),
+                             paste0(toupper(input$batch_id),"_",input$bag_number),
                              input$sample_weight,
                              input$material_type,
-                             input$strain_id,
+                             toupper(input$strain_id),
                              input$operator_name,
                              input$freezer_name,
                              input$shelf_number,
@@ -168,10 +193,10 @@ server <- function(input, output, session) {
         write.csv(ledger_update_db, file = "./app_ledger.csv", row.names = F)
     
         inventory_update_list <- list(input$date_action,
-                                      paste0(input$batch_id,"_",input$bag_number),
-                                      input$batch_id,
+                                      paste0(toupper(input$batch_id),"_",input$bag_number),
+                                      toupper(input$batch_id),
                                       input$bag_number,
-                                      input$strain_id,
+                                      toupper(input$strain_id),
                                       input$material_type,
                                       input$sample_weight, 
                                       input$freezer_name,
@@ -182,8 +207,6 @@ server <- function(input, output, session) {
         
         write.csv(inventory_update_db, file = "./app_inventory.csv", row.names = F)        
         
-        #reload the page to show people things happened
-        #not elegant but functional.
         session$reload()
     
     })
@@ -191,10 +214,9 @@ server <- function(input, output, session) {
     # Modification Update ####
     observeEvent(input$mod_update_entry,{
         
-        # issue ####
-        # need to check for unique bag ID before assigniing
         if(input$mod_unique_batch_id == ""){
             session$reload()
+            #dont love this solution but it works
         }
         
         inventory_update_row <- (inventory_db %>%  
@@ -209,6 +231,18 @@ server <- function(input, output, session) {
         if({input$mod_remove_unique_id}){ 
             inventory_db <- inventory_db %>% subset(.,Unique_Bag_ID != input$mod_unique_batch_id)
             ledger_update_row$Weight = 0 - ledger_update_row$Weight
+            ledger_update_row$Type = "Removal"
+            
+        } else if(input$mod_type == 'Set Balance'){
+            inventory_update_row$Date_Modified = input$mod_date_action
+            inventory_update_row$Weight = input$mod_value_change
+            
+            inventory_db <- inventory_db %>% subset(.,Unique_Bag_ID != input$mod_unique_batch_id)
+            inventory_db <- rbind(inventory_db, inventory_update_row)
+            
+            ledger_update_row$Weight = input$mod_value_change
+            ledger_update_row$Type = "Set Balance"
+            
             
         } else{
             inventory_update_row$Date_Modified = input$mod_date_action
@@ -218,12 +252,16 @@ server <- function(input, output, session) {
             inventory_db <- rbind(inventory_db, inventory_update_row)
             
             ledger_update_row$Weight = ledger_update_row$Weight - input$mod_value_change
+            ledger_update_row$Type = "Removal"
         }
         
-        # Update ledger values and bind row to dataframe: happens even no matter the change.
+        # Update ledger values and bind row to dataframe: happens no matter the change.
         ledger_update_row$Date_Added = input$mod_date_action
         ledger_update_row$Operator = input$mod_operator
+        ledger_update_row$Operator_Group = input$mod_op_group
         ledger_update_row$Purpose = input$mod_reason
+        
+        #drop all of this?
         print('ledger_db')
         print(ncol(ledger_db))
         print(head(ledger_db))
@@ -241,18 +279,73 @@ server <- function(input, output, session) {
     })
     
     # Selectize Updates ####
+    
+    # list of all previous strains
+    updateSelectizeInput(session, "strain_id", 
+                         choices = ledger_db$Strain, 
+                         selected = "", server = TRUE)
+    
     # list of inventory batches
     updateSelectizeInput(session, "mod_unique_batch_id", 
                          choices = inventory_db$Unique_Bag_ID, 
                          selected = "", server = TRUE)
-    # Names for batch changes
+    # Operator for batch modifications
     updateSelectizeInput(session, "mod_operator", 
                          choices = ledger_db$Operator, 
                          selected = "", server = TRUE)
-    # Names for batch additions
+    # Operator for batch entry
     updateSelectizeInput(session, "operator_name", 
                          choices = ledger_db$Operator, 
                          selected = "", server = TRUE)
+    #Freezers for batch entry
+    updateSelectizeInput(session, "freezer_name",
+                         choices = ledger_db$Freezer_ID,
+                         selected ="", server = TRUE)
+    #Operator Group batch modifications
+    updateSelectizeInput(session, "mod_op_group",
+                         choices = ledger_db$Operator_Group,
+                         selected ="", server = TRUE)
+    
+    #Strains for Overview Graph
+    updateSelectizeInput(session, "s_overview_strain_id", 
+                         choices = ledger_db$Strain, 
+                         selected = "", server = TRUE)
+    
+    # Strain Overview ####
+    # current Issue ####
+    # selecting strain breaks thing, needs a date filter.
+    observeEvent(input$s_overview_strain_id,{
+        strain_overview_ggplot <- ledger_db %>% 
+            filter(., ledger_db$Strain == input$s_overview_strain_id) %>% 
+            filter(., !is.na(ledger_db$Date_Added)  || !is.na(ledger_db$Weight)) %>% 
+            ggplot(., aes(x=ledger_db$Date_Added, y = ledger_db$Weight)) +
+            geom_col(stat='identity') +
+            labs(title = "Production of Strain __todo___",
+                 xlab = "Cell Mass Changes",
+                 ylab = "Date Of Modification")
+
+        output$strain_overview_plot <- renderPlot(strain_overview_ggplot)
+        
+        
+    })
+    
+    
+    
+         #take the ledger data table
+    #graph with line for current amount at that time, and bar for inputs/outputs?
+        # filter for date and strain
+     #graph showing additonas and removal? bar chart?
+    # Filter for several strains? Possible?
+    # Weight of current batches in the freezer
+    # number of batches in the freezer, number of bags in the freezer (in text?)
+    
+    # Batch Overview ####
+        # select one batch,
+    # see successive removals
+    # topline freezer thaw numbers
+    # 
+    
+    # Other? ####
 }
 
 # Run the application 
